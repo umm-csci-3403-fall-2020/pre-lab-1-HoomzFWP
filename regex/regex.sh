21e50e0cc729b7f70c1f758078a9ed97dba3524b
hoomzfwp <ndamte@gmail.com>
2020-09-03 00:12:54 -0500

    Create `regex.sh`

---
+++ regex/regex.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#Regex 0
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
+
+#Regex 1
+sed -E  -n 's/\* I am ([a-zA-Z]+). My favorite sandwich is ([a-zA-Z]+)./1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt
+
+#Regex 2
+
+sed -E 's/\* sandwich with ([a-zA-Z.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
+
